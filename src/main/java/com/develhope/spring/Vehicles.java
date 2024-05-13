6f0742368ac0b20afe23245733a5aba7a9ff18db
Ttamer <mericomattia@gmail.com>
2024-05-13 11:21:14 +0200

    create vehicle interface

--- src/main/java/com/develhope/spring/Vehicles.java
+++ src/main/java/com/develhope/spring/Vehicles.java
@@ -2,4 +2,6 @@ package com.develhope.spring;
 
 public interface Vehicles {
 
+    void addToDealer (); // quando verrà aggiunta la classe dealer, questo metodo prenderà un'istanza di Dealer come parametro.
+
 }
