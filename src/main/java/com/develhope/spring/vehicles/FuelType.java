414ebdfe821b4e8c10b88591a7b0f080811f3438
Ttamer <mericomattia@gmail.com>
2024-05-14 19:27:27 +0200

    add created class in correct packages

--- src/main/java/com/develhope/spring/vehicles/FuelType.java
+++
@@ -1,7 +0,0 @@
-package com.develhope.spring.vehicles;
-
-public enum FuelType {
-    GASOLINE,
-    DIESEL,
-    BATTERY
-}
--- src/main/java/com/develhope/spring/vehicles/VehicleState.java
+++
@@ -1,7 +0,0 @@
-package com.develhope.spring.vehicles;
-
-public enum VehicleState {
-    PURCHASABLE,
-    RENTABLE,
-    NOT_AVAILABLE
-}
