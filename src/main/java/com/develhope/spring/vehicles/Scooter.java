279a47c8d13b963c59f0137d188de13ae73656da
Fabrizio Lagioia <meandi958@gmail.com>
2024-05-14 16:47:42 +0200

    aggiunti attributi a Scooter

--- src/main/java/com/develhope/spring/vehicles/Scooter.java
+++ src/main/java/com/develhope/spring/vehicles/Scooter.java
@@ -1,6 +1,21 @@
 package com.develhope.spring.vehicles;
 
-public class Scooter implements Vehicles{
+public class Scooter implements Vehicles {
+    String brand;
+    String model;
+    int engineSize;
+    String color;
+    int power;
+    String gearbox;
+    int registrationYear;
+    //Fueltype fuelType; bisogna creare classe Fueltype
+    double price;
+    double discount;
+    String accesories;
+    boolean isNew;
+    //VehicleState vehicleState; bisogna creare classe VehicleState
+    boolean purchasable;
+    boolean rentable;
 
     @Override
     public void addToDealer() {
