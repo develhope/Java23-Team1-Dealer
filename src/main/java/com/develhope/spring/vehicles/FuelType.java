724dcd5b0ed6ae207f1d1d12fd1f2fe3d1be957b
Ttamer <mericomattia@gmail.com>
2024-05-14 13:14:24 +0200

    create enum classes for vehicles

---
+++ src/main/java/com/develhope/spring/vehicles/FuelType.java
@@ -0,0 +1,7 @@
+package com.develhope.spring.vehicles;
+
+public enum FuelType {
+    GASOLINE,
+    DIESEL,
+    BATTERY
+}
---
+++ src/main/java/com/develhope/spring/vehicles/VehicleState.java
@@ -0,0 +1,7 @@
+package com.develhope.spring.vehicles;
+
+public enum VehicleState {
+    PURCHASABLE,
+    RENTABLE,
+    NOT_AVAILABLE
+}
