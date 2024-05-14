48e36ca65f1288fcd26cd735f72c9a81867a8406
Aurora Scalici <aurorascalici1899@icloud.com>
2024-05-14 20:37:41 +0200

    fix

--- src/main/java/com/develhope/spring/user/model/InvalidEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.develhope.spring.user.model;
-
-public class InvalidEmailException extends Exception {
-    public InvalidEmailException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/develhope/spring/user/model/InvalidValueException.java
+++
@@ -1,7 +0,0 @@
-package com.develhope.spring.user.model;
-
-public class InvalidValueException extends Exception {
-    public InvalidValueException(String message){
-        super(message);
-    }
-}
