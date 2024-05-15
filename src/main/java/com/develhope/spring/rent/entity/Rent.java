d9a1d5fd5e73a9f702f1721134505b293e3a3575
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-05-15 12:30:09 +0200

    Creata Classe Rent

--- src/main/java/com/develhope/spring/rent/entity/Rent.java
+++ src/main/java/com/develhope/spring/rent/entity/Rent.java
@@ -1,4 +1,50 @@
 package com.develhope.spring.rent.entity;
 
-public interface Rent {
+import com.develhope.spring.vehicles.entity.Vehicles;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table
+public class Rent {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Temporal(TemporalType.DATE)
+    private Date startRent, stopRent;
+    private double dailyPrice, totalPryce;
+    private boolean payed;
+    @ManyToOne
+    @JoinColumn(name = "idVehicle", nullable = false)
+    private Vehicles vehicles;
+    public Rent(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public Date getStartRent() {
+        return startRent;
+    }
+
+    public Date getStopRent() {
+        return stopRent;
+    }
+
+    public double getDailyPrice() {
+        return dailyPrice;
+    }
+
+    public double getTotalPryce() {
+        return totalPryce;
+    }
+
+    public boolean isPayed() {
+        return payed;
+    }
+
+    public Vehicles getVehicles() {
+        return vehicles;
+    }
 }
