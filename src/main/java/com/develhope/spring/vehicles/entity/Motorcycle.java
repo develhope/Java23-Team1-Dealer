47516034f769fa7ca52c654472ff094a7d343a56
Fabrizio Lagioia <meandi958@gmail.com>
2024-05-15 19:49:13 +0200

    aggiunta classe motorcycle

---
+++ src/main/java/com/develhope/spring/vehicles/entity/Motorcycle.java
@@ -0,0 +1,22 @@
+package com.develhope.spring.vehicles.entity;
+
+public class Motorcycle implements Vehicles{
+
+    String brand;
+    String model;
+    int engineSize;
+    String color;
+    int power;
+    String gearbox;
+    int registrationYear;
+    FuelType fuelType;
+    double price;
+    double discount;
+    String accessories;
+    boolean isNew;
+    VehicleState vehicleState;
+
+    public void addToDealer () {
+
+    }
+}
