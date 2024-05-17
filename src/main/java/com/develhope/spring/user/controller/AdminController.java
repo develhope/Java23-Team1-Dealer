6018d786f16ac3f61b432ebddfd4376a92c909d8
Aurora Scalici <aurorascalici1899@icloud.com>
2024-05-17 21:36:55 +0200

    create adminContrâ&Serv

---
+++ src/main/java/com/develhope/spring/user/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.develhope.spring.user.controller;
+
+import com.develhope.spring.user.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+}
---
+++ src/main/java/com/develhope/spring/user/service/AdminService.java
@@ -0,0 +1,12 @@
+package com.develhope.spring.user.service;
+
+import com.develhope.spring.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private UserRepository userRepository;
+}
