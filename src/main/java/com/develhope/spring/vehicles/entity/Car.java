7e24da2744a337dcd796f49d5e12719a5c89b3d2
Aurora Scalici <aurorascalici1899@icloud.com>
2024-05-17 20:50:07 +0200

    some fix

--- src/main/java/com/develhope/spring/vehicles/entity/Car.java
+++ src/main/java/com/develhope/spring/vehicles/entity/Car.java
@@ -1,24 +1,23 @@
 package com.develhope.spring.vehicles.entity;
 
 public class Car implements Vehicles {
-    String brand;
-    String model;
-    int engineSize;
-    String color;
-    int power;
-    String gearbox;
-    int registrationYear;
-    FuelType fuelType;
-    double price;
-    double discount;
-    String accessories;
-    boolean isNew;
-    VehicleState vehicleState;
+    private String brand;
+    private String model;
+    private int engineSize;
+    private String color;
+    private int power;
+    private String gearbox;
+    private int registrationYear;
+    private FuelType fuelType;
+    private double price;
+    private double discount;
+    private String accessories;
+    private boolean isNew;
+    private VehicleState vehicleState;
 
 
     @Override
     public void addToDealer() {
 
     }
-
 }
