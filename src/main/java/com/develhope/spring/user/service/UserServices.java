ea7a19534b0aae60715fc4772659420d5a0a3bf7
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-05-22 15:24:59 +0200

    Refactoring

--- src/main/java/com/develhope/spring/user/service/UserServices.java
+++ src/main/java/com/develhope/spring/user/service/UserServices.java
@@ -1,5 +1,7 @@
 package com.develhope.spring.user.service;
 
+import com.develhope.spring.user.entity.User;
+import com.develhope.spring.user.entity.UserKind;
 import com.develhope.spring.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,17 @@ import org.springframework.stereotype.Service;
 public class UserServices {
     @Autowired
     private UserRepository userRepository;
+
+    /**
+     * Verifica se il tipo di utente fornito corrisponde al tipo di utente richiesto.
+     *
+     * @param user l'utente di cui verificare il tipo
+     * @param userKind il tipo di utente richiesto da verificare
+     * @return {@code true} se il tipo di utente dell'utente fornito corrisponde al tipo di utente richiesto,
+     *         {@code false} altrimenti
+     */
+    public boolean checkPermission(User user, UserKind userKind){
+        return user.getUserKind().equals(userKind);
+    }
+
 }
