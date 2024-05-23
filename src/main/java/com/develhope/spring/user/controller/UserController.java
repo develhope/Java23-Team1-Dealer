2b195b76936a1d17991dcdfa03b4a8bd64a1dc99
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-05-23 19:04:34 +0200

    added createProfile, deleteProfile, updateProfile

--- src/main/java/com/develhope/spring/user/controller/UserController.java
+++ src/main/java/com/develhope/spring/user/controller/UserController.java
@@ -1,14 +1,32 @@
 package com.develhope.spring.user.controller;
 
+import com.develhope.spring.user.entity.User;
 import com.develhope.spring.user.service.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
     @Autowired
-    private UserServices userServices;
+    private UserServices userService;
+    @PostMapping("/create")
+    public ResponseEntity<User> createProfile(@RequestBody User user) {
+        User createdUser = userService.createProfile(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteProfile(@PathVariable Long id) {
+        userService.deleteProfile(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<User> updateProfile(@PathVariable Long id, @RequestBody User userDetails) {
+        User updatedUser = userService.updateProfile(id, userDetails);
+        return ResponseEntity.ok(updatedUser);
+    }
+
 }
