6a9f460e5a8bba811cb430f39d92a743927804dd
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-05-23 19:16:53 +0200

    fix

--- src/main/java/com/develhope/spring/user/service/UserServices.java
+++ src/main/java/com/develhope/spring/user/service/UserServices.java
@@ -1,6 +1,7 @@
 package com.develhope.spring.user.service;
 
 import com.develhope.spring.user.entity.User;
+import com.develhope.spring.user.entity.UserKind;
 import com.develhope.spring.user.exception.UserNotFoundException;
 import com.develhope.spring.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,18 @@ public class UserServices {
 
     @Autowired
     private UserRepository userRepository;
+
+    /**
+     * Verifica se il tipo di utente fornito corrisponde al tipo di utente richiesto.
+     *
+     * @param user     l'utente di cui verificare il tipo
+     * @param userKind il tipo di utente richiesto da verificare
+     * @return {@code true} se il tipo di utente dell'utente fornito corrisponde al tipo di utente richiesto,
+     * {@code false} altrimenti
+     */
+    public boolean checkPermission(User user, UserKind userKind) {
+        return user.getUserKind().equals(userKind);
+    }
 
     public User createProfile(User user) {
         return userRepository.save(user);
