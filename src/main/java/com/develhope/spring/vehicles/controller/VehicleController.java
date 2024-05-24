76205b2454c6db1e04ec0eb5bc37a2fa1b8c4bc9
Aurora Scalici <aurorascalici1899@icloud.com>
2024-05-24 10:01:18 +0200

    add body request

--- src/main/java/com/develhope/spring/vehicles/controller/VehicleController.java
+++ src/main/java/com/develhope/spring/vehicles/controller/VehicleController.java
@@ -3,19 +3,21 @@ package com.develhope.spring.vehicles.controller;
 import com.develhope.spring.vehicles.entity.Vehicle;
 import com.develhope.spring.vehicles.service.VehicleServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Vehicles")
+@RequestMapping("/vehicles")
 public class VehicleController {
     @Autowired
     private VehicleServices vehicleServices;
 
     @PostMapping
-    public Vehicle addVehicle(@RequestBody Vehicle vehicle){
-        return vehicleServices.insertVehicle(vehicle);
+    public ResponseEntity<Vehicle> addVehicle(@RequestBody Vehicle vehicle){
+        Vehicle vehicleAdded =vehicleServices.insertVehicle(vehicle);
+        return ResponseEntity.ok(vehicleAdded);
     }
 }
