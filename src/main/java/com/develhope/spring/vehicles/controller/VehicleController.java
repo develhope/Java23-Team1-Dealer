e90ee827b0ed0b0b9ee0631f171ffcfce4367894
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-05-28 15:33:13 +0200

    fix repo to service

--- src/main/java/com/develhope/spring/vehicles/controller/VehicleController.java
+++ src/main/java/com/develhope/spring/vehicles/controller/VehicleController.java
@@ -2,7 +2,7 @@ package com.develhope.spring.vehicles.controller;
 
 import com.develhope.spring.vehicles.entity.Vehicle;
 import com.develhope.spring.vehicles.entity.VehicleState;
-import com.develhope.spring.vehicles.repository.VehicleRepository;
+import com.develhope.spring.vehicles.service.VehicleServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +17,10 @@ import java.util.List;
 @RequestMapping(path = "/vehicle")
 public class VehicleController {
     @Autowired
-    private VehicleRepository vehicleRepository;
+    private VehicleServices vehicleServices;
     @GetMapping
     public ResponseEntity<List<Vehicle>> findByVehicleState(@RequestParam VehicleState vehicleState){
-        List<Vehicle> results = vehicleRepository.findByVehicleState(vehicleState);
+        List<Vehicle> results = vehicleServices.findByVehicleState(vehicleState);
         return ResponseEntity.status(HttpStatus.FOUND).body(results) ;
     }
 }
