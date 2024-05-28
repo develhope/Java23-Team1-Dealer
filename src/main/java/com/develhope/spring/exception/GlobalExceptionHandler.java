6e1349a12e072d4d4e46e78ba96565f9cf3c2a4c
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-05-28 13:15:52 +0200

    Created findByVehicle State

--- src/main/java/com/develhope/spring/exception/GlobalExceptionHandler.java
+++ src/main/java/com/develhope/spring/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.develhope.spring.user.exception;
+package com.develhope.spring.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,5 +13,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NoResultsException.class)
+    public ResponseEntity<String> handleNoResultsException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
