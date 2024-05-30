3ad4c2157c2127438b20ad39022e6bb96edf0816
Fabrizio Lagioia <meandi958@gmail.com>
2024-05-30 19:16:13 +0200

    implementato updateOrderStatus

--- src/main/java/com/develhope/spring/order/service/OrderService.java
+++ src/main/java/com/develhope/spring/order/service/OrderService.java
@@ -1,9 +1,11 @@
 package com.develhope.spring.order.service;
 
 import com.develhope.spring.order.entity.Order;
+import com.develhope.spring.order.entity.OrderStatus;
 import com.develhope.spring.order.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -12,23 +14,38 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-    public Order insert (Order order) {
+    public Order insert(Order order) {
         return orderRepository.save(order);
     }
 
-    public Optional<Order> getById (long id) {
+    public Optional<Order> getById(long id) {
         return orderRepository.findById(id);
     }
 
-    public List<Order> getAll () {
+    public List<Order> getAll() {
         return orderRepository.findAll();
     }
 
-    public void deleteById (long id) {
+    public void deleteById(long id) {
         orderRepository.deleteById(id);
     }
 
-    public void deleteAll () {
+    public void deleteAll() {
         orderRepository.deleteAll();
     }
+
+    public void updateOrderStatus(long orderId) {
+        Order order = orderRepository.findById(orderId).get();
+        if (order.getOrderStatus() == null) {
+            throw new IllegalArgumentException("order status can't be null");
+        } else if (order.getOrderStatus() == OrderStatus.ORDERED) {
+            order.setOrderStatus(OrderStatus.PURCHASED);
+        } else if (order.getOrderStatus() == OrderStatus.PURCHASED) {
+            order.setOrderStatus(OrderStatus.ORDERED);
+        }
+
+
+    }
+
 }
+
