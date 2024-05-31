6ec45813ad7e8578c5c4f5e586feb24cee3a61e4
Aurora Scalici <aurorascalici1899@icloud.com>
2024-05-31 10:23:42 +0200

    create shopVehicleMethod

--- src/main/java/com/develhope/spring/vehicles/service/VehicleService.java
+++ src/main/java/com/develhope/spring/vehicles/service/VehicleService.java
@@ -1,6 +1,7 @@
 package com.develhope.spring.vehicles.service;
 
 import com.develhope.spring.vehicles.entity.Vehicle;
+import com.develhope.spring.vehicles.entity.VehicleState;
 import com.develhope.spring.vehicles.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,14 @@ public class VehicleService {
 
     public Vehicle insertVehicle(Vehicle vehicle) {
         return vehicleRepository.save(vehicle);
+    }
+
+    public boolean shopVehicle(Vehicle vehicle, VehicleState isPurchasable) {
+        if (vehicle.getVehicleState() == isPurchasable || vehicle.isAvailable()) {
+            System.out.println("Vehicle has been successfully purchased!");
+            return true;
+        } else {
+            throw new IllegalArgumentException("Vehicle currently not available, please call our agency for more information.");
+        }
     }
 }
