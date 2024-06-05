e37e383f21f6ed7aefd80e7bcd051033f58f063e
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-06-05 23:44:56 +0200

    patch per presentazione

--- src/main/java/com/develhope/spring/order/entity/Order.java
+++ src/main/java/com/develhope/spring/order/entity/Order.java
@@ -1,6 +1,6 @@
 package com.develhope.spring.order.entity;
 
-import com.develhope.spring.user.entity.User;
+import com.develhope.spring.user.entity.Account;
 import com.develhope.spring.vehicles.entity.Vehicle;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -17,7 +17,7 @@ public class Order {
     @GeneratedValue
     private long id;
     @ManyToOne
-    private User user;
+    private Account account;
     private double deposit;
     private boolean payed;
     @Enumerated(EnumType.STRING)
--- src/main/java/com/develhope/spring/order/service/OrderService.java
+++ src/main/java/com/develhope/spring/order/service/OrderService.java
@@ -1,10 +1,16 @@
 package com.develhope.spring.order.service;
 
+import com.develhope.spring.order.dto.OrderDTO;
+import com.develhope.spring.user.entity.Account;
 import com.develhope.spring.order.entity.Order;
 import com.develhope.spring.order.repository.OrderRepository;
 import com.develhope.spring.exception.OrderNotFoundException;
+import com.develhope.spring.user.repository.UserRepository;
+import com.develhope.spring.vehicles.entity.Vehicle;
+import com.develhope.spring.vehicles.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
@@ -13,8 +19,23 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-    public Order createOrder(Order order) {
-        return orderRepository.save(order);
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    public Order createOrder(OrderDTO orderDTO) {
+        Order orderEntity = new Order();
+        Account account = userRepository.findById(orderDTO.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Vehicle vehicle = vehicleRepository.findById(orderDTO.getVehicleId())
+                .orElseThrow(() -> new RuntimeException("Vehicle not found"));
+        orderEntity.setAccount(account);
+        orderEntity.setVehicle(vehicle);
+        orderEntity.setOrderStatus(orderDTO.getOrderStatus());
+        orderEntity.setDeposit(orderDTO.getDeposit());
+        orderEntity.setPayed(orderDTO.isPayed());
+        return orderRepository.save(orderEntity);
     }
 
     public Order findOrderById(long id) {
@@ -29,7 +50,7 @@ public class OrderService {
         return orderRepository.findAll();
     }
 
-    public Order updateOrder (long id, Order order) {
+    public Order updateOrder(long id, Order order) {
         order.setId(id);
         return orderRepository.save(order);
     }
