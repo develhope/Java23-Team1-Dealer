d98e7af70b8fc5db5aabc5327a3f676fb5a0dcf4
Ttamer <mericomattia@gmail.com>
2024-06-06 14:01:37 +0200

    finished vehicle service

--- src/main/java/com/develhope/spring/vehicles/service/VehicleService.java
+++ src/main/java/com/develhope/spring/vehicles/service/VehicleService.java
@@ -2,9 +2,7 @@ package com.develhope.spring.vehicles.service;
 
 import com.develhope.spring.exception.NoResultsException;
 import com.develhope.spring.exception.VehicleNotFoundException;
-import com.develhope.spring.vehicles.entity.Vehicle;
-import com.develhope.spring.vehicles.entity.VehicleKind;
-import com.develhope.spring.vehicles.entity.VehicleState;
+import com.develhope.spring.vehicles.entity.*;
 import com.develhope.spring.vehicles.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -61,7 +59,124 @@ public class VehicleService {
     }
 
     public List<Vehicle> findByEngineSizeLessThanEqual (int engineSize) {
-        List<Vehicle> vehicles = vehicleRepository.findByModelContaining(model);
+        List<Vehicle> vehicles = vehicleRepository.findByEngineSizeLessThanEqual(engineSize);
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByEngineSizeGreaterThanEqual (int engineSize) {
+        List<Vehicle> vehicles = vehicleRepository.findByEngineSizeGreaterThanEqual(engineSize);
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByColorContaining (String color) {
+        List<Vehicle> vehicles = vehicleRepository.findByColorContaining(color);
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByPowerLessThanEqual (int power) {
+        List<Vehicle> vehicles = vehicleRepository.findByPowerLessThanEqual(power);
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByPowerGreaterThanEqual (int power) {
+        List<Vehicle> vehicles = vehicleRepository.findByPowerGreaterThanEqual(power);
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByGearbox (Gearbox gearbox) {
+        List<Vehicle> vehicles = vehicleRepository.findByGearbox(gearbox);
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByRegistrationYearLessThanEqual (int registrationYear) {
+        List<Vehicle> vehicles = vehicleRepository.findByRegistrationYearLessThanEqual(registrationYear);
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByRegistrationYearGreaterThanEqual (int registrationYear) {
+        List<Vehicle> vehicles = vehicleRepository.findByRegistrationYearGreaterThanEqual(registrationYear);
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByFuelType (FuelType fuelType) {
+        List<Vehicle> vehicles = vehicleRepository.findByFuelType(fuelType);
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByPriceLessThanEqual (int price) {
+        List<Vehicle> vehicles = vehicleRepository.findByPriceLessThanEqual(price);
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByPriceGreaterThanEqual (int price) {
+        List<Vehicle> vehicles = vehicleRepository.findByPriceGreaterThanEqual(price);
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByIsDiscountedTrue () {
+        List<Vehicle> vehicles = vehicleRepository.findByIsDiscountedTrue();
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByIsNewTrue () {
+        List<Vehicle> vehicles = vehicleRepository.findByIsNewTrue();
+        if (vehicles.isEmpty()) {
+            throw new NoResultsException("No results");
+        } else {
+            return vehicles;
+        }
+    }
+
+    public List<Vehicle> findByIsNewFalse () {
+        List<Vehicle> vehicles = vehicleRepository.findByIsNewFalse();
         if (vehicles.isEmpty()) {
             throw new NoResultsException("No results");
         } else {
