1d393fdca37664018512f167f38e03cfb7a68a64
SIlvioVutea <silviovutea@gmail.com>
2024-06-19 11:06:45 +0200

    inserito vehicleDto

--- src/main/java/com/develhope/spring/vehicles/dto/VehicleMapper.java
+++ src/main/java/com/develhope/spring/vehicles/dto/VehicleMapper.java
@@ -1,7 +1,6 @@
 package com.develhope.spring.vehicles.dto;
 
 import com.develhope.spring.vehicles.entity.MostOrderedVehicleModelCount;
-import com.develhope.spring.vehicles.entity.Vehicle;
 
 import org.springframework.stereotype.Component;
 
